fe903e385585f162fd55e5eb89b7c4a56435b2b2
keowen375@gmail.com <keowen375@gmail.com>
2023-05-23 04:35:43 +0300

    Task Complete

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,23 @@
 ## code your solution here. 
+class Cat
+attr_accessor :name
+
+# def initialize(name)
+#     @name = name
+
+# end
+
+
+def name=(name)
+    @name = name
+end
+
+def name
+    @name
+end
+
+def meow
+    puts "meow!"
+end
+
+end
